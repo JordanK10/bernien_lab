50879894a6777c8f5d627db35c574c0d3044a82d
Jordan Kemp <jtk296@gmail.com>
2019-04-07 15:38:50 -0500

    ready to work

---
+++ AtomSortAlgorithms/AtomSim.cpp
@@ -0,0 +1,24 @@
+#include "AtomSim.cpp"
+
+vector<vector<bool>> BalanceCompress::MakeBoolArray(int Dim, float Prob){
+    int i = 0;
+    int j = 0;
+    vector<vector<bool>> matrix;
+    vector<bool> row;
+    while (i < Dim){
+        while(j < Dim){
+            float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+            if (r <= Prob){
+                row.push_back(true);
+            }else{
+                row.push_back(false);
+            }
+            j++;
+        }
+        i++;
+        j = 0;
+        matrix.push_back(row);
+        row = {};
+
+    }
+  
